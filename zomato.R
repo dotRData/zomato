9cee5fb32246a63b60a7b9668c31e32c8f1a2232
Rahul Anand <ranand@groupon.com>
2015-10-12 11:11:12 -0700

    defination for all the process

--- zomato.R
+++ zomato.R
@@ -7,8 +7,9 @@ library(rjson)
 library(tm)
 library(wordcloud)
 
-key     <- '07b65c0fd9ac93e8c6bb905f468eaa14'
-header  <- c(Accept="application/json", 'user_key' = key)
+key     <<- '07b65c0fd9ac93e8c6bb905f468eaa14'
+header  <<- c(Accept="application/json", 'user_key' = key)
+res_id  <<- 16774318
 
 ########################## COMMON ##########################
 
@@ -26,51 +27,106 @@ header  <- c(Accept="application/json", 'user_key' = key)
 ########################## LOCATION ##########################
 
 ##### LOCATION #####
-r_city_name <- 'bangalore'
-lat <- 10
-lon <- 10
-count <- 10
-url_location <- paste('https://developers.zomato.com/api/v2.1/locations?query=', r_city_name, '&lat=', lat, '&lon=', lon, '&count=', count, sep = "")
+location <- function(city_name, 
+                     lat = NULL, 
+                     lon = NULL, 
+                     count = NULL){
+  
+  if (missing(city_name)) stop("Need to specify city_name")
+  
+  url <- paste('https://developers.zomato.com/api/v2.1/locations?query=', city_name,
+               '&lat=', lat, 
+               '&lon=', lon, 
+               '&count=', count, 
+               sep = "")
+  
+  data <- fromJSON(getURL(url, httpheader = header))
+  data
+}
 
 ##### LOCATION DETAILS #####
-r_entity_id <- 4
-r_entity_type <- 'city'
-url_location_details <- paste('https://developers.zomato.com/api/v2.1/location_details?entity_id=', r_entity_id,'&entity_type=', r_entity_type, sep = "")
-
+location_details <- function(entity_id, 
+                             entity_type = c('city', 'subzone', 'zone', 'landmark', 'metro', 'group')){
+  
+  if (missing(entity_id)) stop("Need to specify entity_id")
+  if (missing(entity_type)) stop("Need to specify entity_type in ('city', 'subzone', 'zone', 'landmark', 'metro', 'group')")
+  
+  url <- paste('https://developers.zomato.com/api/v2.1/location_details?entity_id=', entity_id,
+               '&entity_type=', entity_type,
+               sep = "")
+  
+  data <- fromJSON(getURL(url, httpheader = header))
+  data
+}
 ########################## RESTAURANT ##########################
 
 ##### RESTAURANT #####
-r_res_id  <- 16774318
-url_restaurant <- paste('https://developers.zomato.com/api/v2.1/restaurant?res_id=', r_res_id, sep = "")
+resturant <- function(resturant_id){
+  
+  if (missing(resturant_id)) stop("Need to specify resturant_id")
+  
+  url <- paste('https://developers.zomato.com/api/v2.1/restaurant?res_id=', resturant_id, 
+               sep = "")
+  
+  data <- fromJSON(getURL(url, httpheader = header))
+  data
+}
 
 ##### REVIEWS #####
-start <- 1
-count <- 10
-r_res_id  <- 16774318
-url_reviews <- paste('https://developers.zomato.com/api/v2.1/reviews?res_id=', r_res_id, '&start=', start, '&count=',count ,sep = "")
+reviews <- function(resturant_id, 
+                    start = 1,
+                    count = 10){
+  
+  if (missing(resturant_id)) stop("Need to specify resturant_id")
+  
+  url <- paste('https://developers.zomato.com/api/v2.1/reviews?res_id=', resturant_id,
+               '&start=', start,
+               '&count=',count ,sep = "")
+  
+  data <- fromJSON(getURL(url, httpheader = header))
+  data
+}
 
 ##### SEARCH #####
-# entity_type <- c('city', 'subzone', 'zone', 'landmark', 'metro', 'group')
-# sort <- c('cost', 'rating', 'real_distance')
-# order <- c('asc', 'desc')
-entity_id <- 4
-entity_type <- 'city'
-q <- 
-start <- 
-count <- 
-lat <- 
-lon <- 
-radius <- 
-cuisines <- 
-establishment_type <- 
-collection_id <- 
-r_sort <- 'rating'
-r_order <- 'desc'
-url_search <- paste('https://developers.zomato.com/api/v2.1/search?entity_type=', r_entity_type,'&sort=', r_sort,'&order=', r_order, sep = "")
-
+search <- function(entity_id = NULL, 
+                   entity_type = c('city', 'subzone', 'zone', 'landmark', 'metro', 'group'),
+                   q = NULL, 
+                   start = 1,
+                   count = 10,
+                   lat = NULL,
+                   lon = NULL,
+                   radius = NULL,
+                   cuisines = NULL,
+                   establishment_type = NULL, 
+                   collection_id  = NULL,
+                   sort = c('cost', 'rating', 'real_distance'),
+                   order = c('asc', 'desc')){
+  
+  if (missing(entity_type)) stop("Need to specify entity_type in ('city', 'subzone', 'zone', 'landmark', 'metro', 'group')")
+  if (missing(sort)) stop("Need to specify sort in ('cost', 'rating', 'real_distance')")
+  if (missing(order)) stop("Need to specify order in ('asc', 'desc')")
+  
+  url <- paste('https://developers.zomato.com/api/v2.1/search?entity_id=', entity_id, 
+               '&entity_type=', entity_type,
+               '&q=', q,
+               '&start=', start, 
+               '&count=', count,
+               '&lat=', lat,
+               '&lon=', lon,
+               '&radius=', radius,
+               '&cuisines=', cuisines,
+               '&establishment_type=', establishment_type, 
+               '&collection_id=', collection_id,
+               '&sort=', sort, 
+               '&order=', order, 
+               sep = "")
+  
+  data <- fromJSON(getURL(url, httpheader = header))
+  data
+}
 ##############################################################################
 
-data    <- fromJSON(getURL(url, httpheader = header))
+data    <- reviews(resturant_id = 16774318)
 
 for(i in 1:length(data$user_reviews)){
     print(data$user_reviews[[i]]$review$review_text)
